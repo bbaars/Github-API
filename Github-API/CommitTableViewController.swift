52ae4c61a7065df7578cc924d1f89156e2cf1edb
Brandon Baars <brandonbaars@gmail.com>
2020-12-12 19:44:53 -0500

    Got rid of storyboard and added the initial table controller

--- Github-API/CommitTableViewController.swift
+++ Github-API/CommitTableViewController.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CommitTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        title = "Vapor Commits"
     }
-
-
 }
 
